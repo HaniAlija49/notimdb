69a707f8a1d13ae413f0d3e20971734e1adc7031
UvejsMurtezi <um30025@seeu.edu.mk>
2024-01-13 12:47:24 +0100

    Actor Entity created

--- src/main/java/com/notimdb/notimdb/repository/ActorRepository.java
+++ src/main/java/com/notimdb/notimdb/repository/ActorRepository.java
@@ -1,6 +1,7 @@
 package com.notimdb.notimdb.repository;
 
+import com.notimdb.notimdb.pojo.entity.Actor;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ActorRepository extends CrudRepository <Actor, Integer > {
+public interface ActorRepository extends CrudRepository <Actor, Integer> {
 }
